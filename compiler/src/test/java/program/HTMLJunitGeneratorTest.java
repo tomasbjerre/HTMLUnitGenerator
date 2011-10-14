41f8d7889c3285c9be9a2e7f141fbad5ccec8ae5
Tomas Bjerre <tomasbjerre@yahoo.com>
2011-10-14 14:58:21 +0200

    Moving test cases

---
+++ compiler/src/test/java/program/HTMLJunitGeneratorTest.java
@@ -0,0 +1,36 @@
+package program;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import compiler.data.Flow;
+import compiler.generator.htmljunit.HTMLJunitGenerator;
+
+public class HTMLJunitGeneratorTest extends TestCase {
+
+	private HTMLJunitGenerator generator;
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		Flow flow = new Flow();
+		generator = new HTMLJunitGenerator(flow,"FileName");
+	}
+
+	@Test
+	public void testEscapeString1() {
+		String input = "//*[@id=\"_eventId_search\"]";
+		assertEquals("//*[@id=\\\"_eventId_search\\\"]",generator.escapeString(input));
+		assertEquals("//*[@id=\"_eventId_search\"]",input);
+	}
+
+	@Test
+	public void testEscapeString2() {
+		String input = "/html/body/div[2]/div/div[2]/div[2]/div/div[3]";
+		assertEquals("/html/body/div[2]/div/div[2]/div[2]/div/div[3]",generator.escapeString(input));
+		assertEquals("/html/body/div[2]/div/div[2]/div[2]/div/div[3]",input);
+	}
+
+}
